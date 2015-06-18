1302421f613ee12f62f6610fe50ff98610d41000
Laura L Watkins <lauralwatkins@gmail.com>
2015-06-18 11:27:39 -0400

    Add routine to calculate circular velocity.

---
+++ vcirc.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+# GENHERNQUIST.VCIRC
+# Laura L Watkins [lauralwatkins@gmail.com]
+# -----------------------------------------------------------------------------
+
+from numpy import *
+from astropy import units as u, constants as c
+from scipy import special
+from encmass import encmass
+
+
+def vcirc(r, norm, rs, alpha, beta, gamma):
+    
+    """
+    Enclosed mass profile of a generalised Hernquist model.
+    
+    INPUTS
+      r     : radial variable (requires unit)
+      norm  : normalisation (requires unit)
+      rs    : scale radius of model (requires unit)
+      alpha : sharpness of transition between inner and outer
+      beta  : outer logarithmic slope
+      gamma : inner logarithmic slope
+    """
+    
+    mass = encmass(r, norm, rs, alpha, beta, gamma)
+    
+    vcirc = (sqrt(c.G*mass/r)).to("km/s")
+    
+    return vcirc
