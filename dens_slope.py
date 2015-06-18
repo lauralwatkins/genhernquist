f8bf5d7ee7617b1600130c2b832b3704d7c75c5e
Laura L Watkins <lauralwatkins@gmail.com>
2015-06-18 11:27:27 -0400

    Add routine to calculate density slope.

---
+++ dens_slope.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+# GENHERNQUIST.DENS_SLOPE
+# Laura L Watkins [lauralwatkins@gmail.com]
+# -----------------------------------------------------------------------------
+
+
+def dens_slope(r, rs, alpha, beta, gamma):
+    
+    """
+    The slope of the density profile of a generalised Hernquist model.
+    
+    INPUTS
+      r     : radial variable (requires unit)
+      rs    : scale radius of model (requires unit)
+      alpha : sharpness of transition between inner and outer
+      beta  : outer logarithmic slope
+      gamma : inner logarithmic slope
+    """
+    
+    slope = -gamma + (gamma-beta)*r**alpha/(rs**alpha + r**alpha)
+    
+    return slope
